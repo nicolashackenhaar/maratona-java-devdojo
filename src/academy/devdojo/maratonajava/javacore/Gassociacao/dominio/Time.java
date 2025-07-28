f10a7ce971a22afb7f588672d539559d8d9be12e
Nicolas Hack <nicolashackenhaar@gmail.com>
2025-07-28 16:32:53 -0300

    Aula 65 Associação  - Associação unidirecional

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/dominio/Time.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.dominio;
+
+public class Time {
+    private String nome;
+
+    public Time(String nome) {
+        this.nome = nome;
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
