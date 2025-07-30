f0fb0ad377f4be019357cabc0838abd0182a6b0c
Nicolas Hack <nicolashackenhaar@gmail.com>
2025-07-29 23:27:25 -0300

    Arruma verificação de divisao

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora {
     }
 
     public double divideDoisNumeros(double num1, double num2) {
-        if (num2 != 0) {
+        if (num1 != 0|| num2 != 0) {
             return num1 / num2;
         }
 
